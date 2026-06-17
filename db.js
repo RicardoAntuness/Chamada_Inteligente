43734cfe38b6912f8793596c34637c2084b52610
Ricardo Rigo Antunes <ricardorigoantunes@users.noreply.github.com>
2026-06-17 18:48:54 -0300

    ajustes backend

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
     user: "admin",
-    password: "admin",
+    password: "admin", // <--- Mude de "1234" para "admin"
     host: "localhost",
     port: 5432,
     database: "chamada"
