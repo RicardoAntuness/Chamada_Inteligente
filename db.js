13fb4333dfeb20b75bff6c176652f416a59f2c53
Ricardo Rigo Antunes <ricardorigoantunes@users.noreply.github.com>
2026-06-17 18:40:32 -0300

    ajustes backend

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const { Pool } = require("pg");
 const pool = new Pool({
     user: "admin",
     password: "1234",
-    host: "postgres",
+    host: "localhost",
     port: 5432,
     database: "chamada"
 });
