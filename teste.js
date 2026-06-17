c39d3b3e989c366a489b5dc40286427edbd36f90
Ricardo Rigo Antunes <ricardorigoantunes@users.noreply.github.com>
2026-06-17 20:31:45 -0300

    ajustes frontend

--- teste.js
+++ teste.js
@@ -1,14 +1,32 @@
-const pool = require("./db");
+const express = require("express");
 
-async function test() {
-    try {
-        const result = await pool.query("SELECT current_database(), current_user");
-        console.log(result.rows);
-    } catch (err) {
-        console.error(err);
-    } finally {
-        process.exit();
-    }
-}
+const app = express();
 
-test();
+app.use(express.json());
+
+app.get("/ping", (req, res) => {
+    console.log("PING RECEBIDO");
+    res.send("pong");
+});
+
+app.post("/aula/iniciar", (req, res) => {
+    console.log("AULA INICIADA");
+    res.json({
+        mensagem: "Aula iniciada com sucesso!",
+        inicio: Date.now()
+    });
+});
+
+app.get("/alunos", (req, res) => {
+    res.json([
+        {
+            id: 1,
+            nome: "Ricardo",
+            uid: "123ABC"
+        }
+    ]);
+});
+
+app.listen(3000, "0.0.0.0", () => {
+    console.log("Servidor de teste rodando na porta 3000");
+});
